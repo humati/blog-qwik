e7fd0f68e4e19706e2d670fb26bf83933fe14a16
tuanhuynhjk <tuan.huynh@joikid.com>
2023-05-25 06:22:35 +0700

    init qwik project

---
+++ src/routes/home/index.tsx
@@ -0,0 +1,10 @@
+import { component$ } from '@builder.io/qwik';
+import { BlogLayout } from '~/components/blog/blog-layout';
+
+export default component$(() => {
+  return (
+    <>
+      <BlogLayout></BlogLayout>
+    </>
+  );
+});
